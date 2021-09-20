13e591749d770bcef99b8bd2432faa49283704d3
CodeCinteriors <91065284+CodeCinteriors@users.noreply.github.com>
2021-09-20 19:00:43 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,71 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-360a0001",
+      "name": "360A0001",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0.001176594599993308,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7585993828644284,
+          "pitch": 0.042780817718419684,
+          "rotation": 0,
+          "target": "1-360a0001-_1_"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-360a0001-_1_",
+      "name": "360A0001-_1_",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 500,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.0691288715634846,
+          "pitch": 0.1006657253849319,
+          "rotation": 0,
+          "target": "0-360a0001"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Bedroom02-AK Block",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
